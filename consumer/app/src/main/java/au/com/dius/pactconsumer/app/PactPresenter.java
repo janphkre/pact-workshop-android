0dd47125fb9f9ae8c3acbb078ab50b3dcf5d3cf0
Jan Phillip Kretzschmar <janphkre@gmx.de>
2019-07-11 09:28:48 +0200

    Update all gradle files to recent versions, Removed all classes from example

--- consumer/app/src/main/java/au/com/dius/pactconsumer/app/PactPresenter.java
+++
@@ -1,7 +0,0 @@
-package au.com.dius.pactconsumer.app;
-
-
-public interface PactPresenter {
-  void onStart();
-  void onStop();
-}
---
+++ consumer/app/src/main/java/au/com/dius/pactconsumer/presentation/MainActivity.kt
@@ -0,0 +1,15 @@
+package au.com.dius.pactconsumer.presentation
+
+import android.app.Activity
+import android.os.Bundle
+import au.com.dius.pactconsumer.R
+
+class MainActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activtiy_animals)
+    }
+
+    //TODO
+}
